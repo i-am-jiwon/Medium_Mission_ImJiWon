2c2e4288598fac9edb1d01e693266c2f4fbfda80
i-am-jiwon <dlark1128@gmail.com>
2023-12-11 11:49:13 +0900

    valid join form

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,8 +1,13 @@
 package com.ll.medium.domain.member.member.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +17,22 @@ public class MemberController {
     @GetMapping("/join")
     public String showJoin(){
         return "domain/member/member/join";
+    }
+
+    @Getter
+    @Setter
+    public static class JoinForm{
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+
+    }
+
+    @PostMapping("/join")
+    public String signup(@Valid JoinForm joinForm) {
+
+
+        return "redirect:/";
     }
 }
